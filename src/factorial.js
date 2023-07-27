1ad9007cffa4d4e4926062fe710ab51ee3467a55
Aviruk Basak <avirukbasak@gmail.com>
2023-07-27 13:42:20 +0530

    Update factorial.js
    Made it incorrect

--- src/factorial.js
+++ src/factorial.js
@@ -1,6 +1,7 @@
 const factorial = function(n, acc = 1) {
   if (n <= 1) return acc;
-  return factorial(n-1, acc * n);
+  // this line is incorrect, fix it
+  return factorial(n, acc * n);
 }
 
 console.log(factorial(5));
