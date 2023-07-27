e18eac29403bd47a91fa961e1a139804ee6d72be
NWOM <98202572+NWOM@users.noreply.github.com>
2023-07-27 13:50:17 +0530

    Update factorial.js

--- src/factorial.js
+++ src/factorial.js
@@ -1,7 +1,7 @@
 const factorial = function(n, acc = 1) {
   if (n <= 1) return acc;
   // this line is incorrect, fix it
-  return factorial(n, acc * n);
+  return factorial(n-1, acc * n);
 }
 
 console.log(factorial(5));
